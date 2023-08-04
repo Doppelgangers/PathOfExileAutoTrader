acc2f38e64fb76a91c7efad63d8602cd027ded9b
Doppelgangers <6666genocide666@mail.ru>
2023-08-04 23:38:34 +0300

    create class Parser_poe_log

--- modules/game_log_reader/log_parser.py
+++ modules/game_log_reader/log_parser.py
@@ -1,37 +1,50 @@
 
 
-def parse_trades_for_regular_tab(message: str) -> dict:
-    message = message.split("@От кого ")[1].split(":")
-    name = message[0]
-    if len(name.split(" ")) > 1:
-        name = name.split(" ")[1]
-    message = "".join(message[1:]).split("купить у вас ")[1]
-    message = message.split(" за ")
-    item = message[0].replace(",", "")
-    message = message[1].split(" в лиге ")
-    price, orb = message[0].split(" ")
-    message = message[1].split(" (секция \"")
-    league = message[0]
-    message = message[1].split("\"; позиция ")
-    section = message[0]
-    message = message[1].split(" столбец, ")
-    left = message[0]
-    top = message[1].split(' ')[0]
-    print(f"""
-    Покупатель: {name}
-    Покупает: {item}
-    За: {price} - {orb}
-    В лиге: {league}
-    Находится по координатам (left: {left}, top: {top}) в секции {section}
-    """)
-    data = {"name": name,
-            "item": item,
-            "price": price,
-            "orb": orb,
-            "league": league,
-            "section": section,
-            "left": left,
-            "top": top
-            }
-    return data
+class Parser_poe_log:
 
+    @staticmethod
+    def parse_trades_for_regular_tab(message: str) -> dict:
+        message = message.split("@От кого ")[1].split(":")
+        name = message[0]
+        if len(name.split(" ")) > 1:
+            name = name.split(" ")[1]
+        message = "".join(message[1:]).split("купить у вас ")[1]
+        message = message.split(" за ")
+        item = message[0].replace(",", "")
+        message = message[1].split(" в лиге ")
+        price, orb = message[0].split(" ")
+        message = message[1].split(" (секция \"")
+        league = message[0]
+        message = message[1].split("\"; позиция ")
+        section = message[0]
+        message = message[1].split(" столбец, ")
+        left = message[0]
+        top = message[1].split(' ')[0]
+
+        return {"name": name,
+                "item": item,
+                "price": price,
+                "orb": orb,
+                "league": league,
+                "section": section,
+                "left": left,
+                "top": top
+                }
+
+
+"///TODO: проверить разделение по словам на разных языках"
+# import re
+#
+# text = "Hello world! This is a sample text."
+#
+# # List of possible words to split the text
+# possible_words = ["world", "sample"]
+#
+# # Create a regular expression pattern by joining the possible words with the "|" OR operator
+# pattern = r"\b(" + "|".join(map(re.escape, possible_words)) + r")\b"
+#
+# # Split the text using the pattern
+# result = re.split(pattern, text)
+#
+# # Print the result
+# print(result)
--- modules/game_log_reader/log_read.py
+++ modules/game_log_reader/log_read.py
@@ -1,7 +1,9 @@
 import tailer
 from settings import logfile_path
+
 while True:
     for line in tailer.follow(open(logfile_path, encoding="UTF-8")):
         log = line.strip()
+
 
 
