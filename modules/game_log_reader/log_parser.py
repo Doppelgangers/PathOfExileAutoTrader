0f72fe2c974a3a7881df64ef81d0463c925a8b2d
Doppelgangers <6666genocide666@mail.ru>
2023-08-05 20:51:01 +0300

    added support for two languages to the parser, and also rewrote it using regular expressions

--- modules/game_log_reader/log_parser.py
+++ modules/game_log_reader/log_parser.py
@@ -1,25 +1,26 @@
+import re
 
 
 class Parser_poe_log:
+    message_en = """2023/08/05 20:44:10 23767031 cffb0719 [INFO Client 7156] @От кого <•ÐiS•> Snow_Fairy: Hi, I would like to buy your Great Old One's Ward Corrugated Buckler listed for 1 alch in Crucible (stash tab "for sale 3"; position: left 6, top 9)"""
 
     @staticmethod
-    def parse_trades_for_regular_tab(message: str) -> dict:
-        message = message.split("@От кого ")[1].split(":")
-        name = message[0]
-        if len(name.split(" ")) > 1:
-            name = name.split(" ")[1]
-        message = "".join(message[1:]).split("купить у вас ")[1]
-        message = message.split(" за ")
-        item = message[0].replace(",", "")
-        message = message[1].split(" в лиге ")
-        price, orb = message[0].split(" ")
-        message = message[1].split(" (секция \"")
-        league = message[0]
-        message = message[1].split("\"; позиция ")
-        section = message[0]
-        message = message[1].split(" столбец, ")
-        left = message[0]
-        top = message[1].split(' ')[0]
+    def parse_trades_for_regular_tab(message: str) -> dict | None:
+        rus_pattern = """@От кого (\S+)(?: (\S+))?: Здравствуйте, хочу купить у вас (.+) за ([0-9]+) (.+) в лиге (.+) \(секция "(.+)"; позиция: (\d+) столбец, (\d+) ряд"""
+        en_pattern =  """@От кого (\S+)(?: (\S+))?: Hi, I would like to buy your (.+) listed for ([0-9]+) (.+) in (.+) \(stash tab "(.+)"; position: left (\d+), top (\d+)"""
+        match = re.findall(rus_pattern, message)
+        match2 = re.findall(en_pattern, message)
+        if not match and not match2:
+            return None
+        match = match[0] if match else match2[0]
+        name = match[0] if match[1] == '' else match[1]
+        item = match[2]
+        price = match[3]
+        orb = match[4]
+        league = match[5]
+        section = match[6]
+        left = match[7]
+        top = match[8]
 
         return {"name": name,
                 "item": item,
@@ -32,19 +33,8 @@ class Parser_poe_log:
                 }
 
 
-"///TODO: проверить разделение по словам на разных языках"
-# import re
-#
-# text = "Hello world! This is a sample text."
-#
-# # List of possible words to split the text
-# possible_words = ["world", "sample"]
-#
-# # Create a regular expression pattern by joining the possible words with the "|" OR operator
-# pattern = r"\b(" + "|".join(map(re.escape, possible_words)) + r")\b"
-#
-# # Split the text using the pattern
-# result = re.split(pattern, text)
-#
-# # Print the result
-# print(result)
+if __name__ == "__main__":
+    p = Parser_poe_log
+    message =      '2023/08/05 18:38:18 16215546 cffb0719 [INFO Client 2488] @От кого DIS Snow_Fairy: Здравствуйте, хочу купить у вас Естественная иерархия Талисман перогнильца за 100 alch в лиге Crucible (секция "Sale"; позиция: 11 столбец, 21 ряд)'
+    message_en =  """2023/08/05 20:44:10 23767031 cffb0719 [INFO Client 7156] @От кого <•ÐiS•> Snow_Fairy: Hi, I would like to buy your Great Old One's Ward Corrugated Buckler listed for 1 alch in Crucible (stash tab "for sale 3"; position: left 6, top 9)"""
+    print(p.parse_trades_for_regular_tab(message_en))
--- modules/game_log_reader/log_read.py
+++ modules/game_log_reader/log_read.py
@@ -4,6 +4,8 @@ from settings import logfile_path
 while True:
     for line in tailer.follow(open(logfile_path, encoding="UTF-8")):
         log = line.strip()
+        print(log)
+
 
 
 
